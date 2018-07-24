23095b5262828a96240f1e27a4e7a557035e632b
mityagz <mityagz@gmail.com>
2018-07-24 06:22:07 -0400

    task 4.4

---
+++ exercise/04/task_4_4.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 4.4
+Из строк command1 и command2 получить список VLANов,
+которые есть и в команде command1 и в команде command2.
+Для данного примера, результатом должен быть список: [1, 3, 100]
+Этот список содержит подсказку по типу итоговых данных.
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+'''
+
+command1 = 'switchport trunk allowed vlan 1,3,10,20,30,100'
+command2 = 'switchport trunk allowed vlan 1,3,100,200,300'
+
+
+In [341]: s = set([ int(vlan) for vlan in command1.split()[4].split(',') if vlan.isdigit() ]) & set([ int(vlan) for vlan in command2.split()[4].split(',') if vlan.isd
+     ...: igit() ])
+
+In [342]: s
+Out[342]: {1, 3, 100}

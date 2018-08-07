00c6f169de690235b3f0d8bf86089b5987260916
mityagz <mityagz@gmail.com>
2018-08-07 09:23:01 -0400

    6

---
+++ exercise/06/62.py
@@ -0,0 +1,22 @@
+#!/home/python/n/pyneng/bin/python
+# -*- coding: utf-8 -*-
+
+
+'''
+Задание 6.2
+Список mac содержит MAC-адреса в формате XXXX:XXXX:XXXX.
+Однако, в оборудовании cisco MAC-адреса используются в формате XXXX.XXXX.XXXX.
+Создать скрипт, который преобразует MAC-адреса в формат cisco
+и добавляет их в новый список mac_cisco
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+'''
+
+mac = ['aabb:cc80:7000', 'aabb:dd80:7340', 'aabb:ee80:7000', 'aabb:ff80:7000']
+
+mac_cisco = []
+
+for m in mac:
+ mac_cisco.append(".".join((m.split(":"))))
+
+print(mac)
+print(mac_cisco)

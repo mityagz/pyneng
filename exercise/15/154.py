81ccd2dd6e1695df5d27308bf6521b1ad16a6da5
mityagz <mityagz@gmail.com>
2018-09-10 01:44:34 -0400

    15

--- exercise/15/154.py
+++ exercise/15/154.py
@@ -15,7 +15,7 @@ res = []
 def parse_sh_ip_int_br(filename):
  with open(filename) as f:
    for l in f:
-     match = re.match('(?P<intf>\S+)\s+(?P<ip>(?:(?:\d+){1,3}.){3}(?:\d+){1,3})\s+\S+\s+\S+\s+(?P<status>up|down)\s+(?P<proto>up|down)', l)
+     match = re.match('(?P<intf>\S+)\s+(?P<ip>(?:(?:(?:\d+){1,3}.){3}(?:\d+){1,3})|unassigned)\s+\S+\s+\S+\s+(?P<status>up|.*down)\s+(?P<proto>up|down)', l)
      if match:
          res.append(match.group('intf', 'ip', 'status', 'proto'))
  return res
@@ -25,4 +25,5 @@ if __name__ == '__main__':
 
 
 #./154.py sh_ip_int_br_2.txt 
-#[('FastEthernet0/0', '15.0.15.1', 'up', 'up'), ('FastEthernet0/1', '10.0.12.1', 'up', 'up'), ('FastEthernet0/2', '10.0.13.1', 'up', 'up'), ('Loopback0', '10.1.1.1', 'up', 'up'), ('Loopback100', '100.0.0.1', 'up', 'up')]
+#[('FastEthernet0/0', '15.0.15.1', 'up', 'up'), ('FastEthernet0/1', '10.0.12.1', 'up', 'up'),  \
+# ('FastEthernet0/2', '10.0.13.1', 'up', 'up'), ('FastEthernet0/3', 'unassigned', 'administratively down', 'down'), ('Loopback0', '10.1.1.1', 'up', 'up'), ('Loopback100', '100.0.0.1', 'up', 'up')]

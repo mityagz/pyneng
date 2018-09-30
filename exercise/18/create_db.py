2dca9fef43f894a466832ad2abc7922afd0ae8a4
mityagz <mityagz@gmail.com>
2018-09-30 03:56:11 -0400

    18

--- exercise/18/create_db.py
+++ exercise/18/create_db.py
@@ -17,3 +17,43 @@ create_db.py
 
 db_filename = 'dhcp_snooping.db'
 schema_filename = 'dhcp_snooping_schema.sql'
+
+import sqlite3
+import re
+import os
+
+regex = re.compile('(\S+) +(\S+) +\d+ +\S+ +(\d+) +(\S+)')
+result = []
+
+
+data_filename = 'dhcp_snooping.txt'
+db_filename = 'dhcp_snooping.db'
+schema_filename = 'dhcp_snooping_schema.sql'
+ 
+
+def is_db_exist():
+ db_exists = os.path.exists(db_filename)
+ if db_exists:
+  return True
+
+def create_db(db, schema):
+ print('Creating schema...')
+ with open(schema, 'r') as f:
+  conn = sqlite3.connect(db)
+  schema = f.read(); 
+  conn.executescript(schema)
+  conn.close()
+ if is_db_exist():
+  print('Done')
+ else:
+  print('Failure create db')
+
+
+
+if __name__ == '__main__':
+ if not is_db_exist():
+  create_db(db_filename, schema_filename)
+ else:
+  print('Database exists, assume dhcp table does, too.')
+
+ 
